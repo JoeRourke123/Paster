e02b6fcbb931361e409744d56753187b6fa9a5c6
seany067 <sean@escreet.co.uk>
2019-12-07 12:52:38 +0000

    Added drag and drop to text box

--- client/src/main/java/com/pasteyboi/client/Client.java
+++ client/src/main/java/com/pasteyboi/client/Client.java
@@ -42,7 +42,7 @@ public class Client extends Application {
         login.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                
+
             }
         });
 
