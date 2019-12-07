9a0a7864d9d4a26f1b0577e1858824aa443f77c8
seany067 <sean@escreet.co.uk>
2019-12-07 12:51:23 +0000

    Added drag and drop to text box

--- client/src/main/java/com/pasteyboi/client/Client.java
+++ client/src/main/java/com/pasteyboi/client/Client.java
@@ -1,6 +1,8 @@
 package main.java.com.pasteyboi.client;
 
 import javafx.application.Application;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import javafx.scene.Scene;
@@ -8,9 +10,13 @@ import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.DragEvent;
+
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+import java.util.Scanner;
 
 public class Client extends Application {
 
@@ -36,7 +42,7 @@ public class Client extends Application {
         login.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                
             }
         });
 
@@ -51,20 +57,51 @@ public class Client extends Application {
     public Scene textDump() {
         VBox root = new VBox(5);
 
-
         StackPane dump = new StackPane();
-        Label text = new Label("LKFJAL;KDGF");
-        TextField target = new TextField();
+        Label text = new Label("Enter text or drag and drop files");
+        TextArea target = new TextArea();
+        target.setPrefSize(500,500);
         target.setOnDragOver(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
-                System.out.println(event.getGestureSource() + "___" + event.getDragboard());
+                if(event.getGestureSource() != target && event.getDragboard().hasFiles()) {
+                    event.acceptTransferModes(TransferMode.ANY);
+                }
+                event.consume();
+            }
+        });
+        target.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if(db.hasFiles()) {
+                    target.appendText(getFiles(db.getFiles()));
+                    success = true;
+                }
+                event.setDropCompleted(success);
+                event.consume();
             }
         });
         root.getChildren().addAll(target, text);
 
         Scene scene = new Scene(root);
         return scene;
+    }
+
+    public String getFiles(Collection<File> path) {
+        String out = "";
+        for (File file : path) {
+            try {
+                Scanner fileread = new Scanner(file);
+                while(fileread.hasNextLine()) {
+                    out += fileread.nextLine() + "\n";
+                }
+            }catch(Exception e) {
+                System.out.println("Not a text file");
+            }
+        }
+        return out;
     }
 
     public String hashPass(String pass) throws NoSuchAlgorithmException {
